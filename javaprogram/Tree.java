5abed55eb99c4ae64f6bbf5d0aac4fb34285a313
Caroline <carol-html@hotmail.com>
2025-01-14 19:36:25 -0300

    Create java program Tree

---
+++ javaprogram/Tree.java
@@ -0,0 +1,36 @@
+package javaprogram; 
+
+public class Tree {
+        public static void main(String[] args){
+    
+        // Introducing myself to users and planting a tree for them! 
+    
+        System.out.println("Hello, I´m Caroline and I´m learning to program in Java!");
+        System.out.println("I'm gonna planting a tree today!");
+    
+        System.out.println("         ");
+        System.out.println("    *    ");
+        System.out.println("   ***   ");
+        System.out.println("   ***   ");
+        System.out.println("    *    ");
+        System.out.println("    *    ");
+        System.out.println("         ");
+        System.out.println("  * ** *  ");
+        System.out.println("  ******  ");
+        System.out.println("   ****   ");
+        System.out.println("    **    ");
+        System.out.println("    **    ");
+        System.out.println("~~~~~~~~~~");
+        System.out.println("          ");
+        System.out.println("  *****  ");
+        System.out.println("  *****  ");
+        System.out.println("  *****  ");
+        System.out.println("    *    ");
+        System.out.println("    *    ");
+        System.out.println("#########");
+    
+    
+        }
+    }
+
+
